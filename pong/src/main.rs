9f71c422ab154a538345e7f00dc04f0a9ab72f2a
fjoanis <frederic.joanis@gmail.com>
2018-12-26 20:58:13 -0500

    Finished pong tutorial

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -1,35 +1,56 @@
 extern crate amethyst;
 
 use amethyst::{
+    core::transform::TransformBundle,
     prelude::*,
-    renderer::{DisplayConfig, DrawFlat, Pipeline, PosNormTex, RenderBundle, Stage},
+    renderer::{DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage},
+    ui::{DrawUi, UiBundle},
     utils::application_root_dir,
 };
 
-struct Example;
-
-impl SimpleState for Example {}
+mod pong;
+mod systems;
 
 fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
+    // We'll put the rest of the code here.
 
-    let path = format!(
-        "{}/resources/display_config.ron",
-        application_root_dir()
-    );
+    use crate::pong::Pong;
+
+    let path = format!("{}/resources/display_config.ron", application_root_dir());
+
     let config = DisplayConfig::load(&path);
 
     let pipe = Pipeline::build().with_stage(
         Stage::with_backbuffer()
-            .clear_target([0.00196, 0.23726, 0.21765, 1.0], 1.0)
-            .with_pass(DrawFlat::<PosNormTex>::new()),
+            .clear_target([0.0, 0.0, 0.0, 1.0], 1.0)
+            .with_pass(DrawFlat2D::new())
+            .with_pass(DrawUi::new()),
     );
 
-    let game_data =
-        GameDataBuilder::default().with_bundle(RenderBundle::new(pipe, Some(config)))?;
-    let mut game = Application::new("./", Example, game_data)?;
+    use amethyst::input::InputBundle;
+
+    let binding_path = format!("{}/resources/bindings_config.ron", application_root_dir());
+
+    let input_bundle =
+        InputBundle::<String, String>::new().with_bindings_from_file(binding_path)?;
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(input_bundle)?
+        .with_bundle(UiBundle::<String, String>::new())?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(
+            systems::BounceSystem,
+            "collision_system",
+            &["paddle_system", "ball_system"],
+        )
+        .with(systems::WinnerSystem, "winner_system", &["ball_system"]);
+
+    let mut game = Application::new("./", Pong, game_data)?;
 
     game.run();
-
     Ok(())
 }
