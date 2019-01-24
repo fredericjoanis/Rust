f20a02a6e03a9d0cf601632f01d97175bb8c7aa4
fjoanis <frederic.joanis@gmail.com>
2019-01-23 19:33:52 -0500

    Fix bouncing bug.

--- pong/src/systems/bounce.rs
+++ pong/src/systems/bounce.rs
@@ -24,9 +24,9 @@ impl<'s> System<'s> for BounceSystem {
             let ball_y = transform.translation().y;
 
             // Bounce at the top or the bottom of the arena.
-            if ball_y >= ARENA_HEIGHT - ball.radius && ball.velocity[1] > 0.0 {
-                ball.velocity[1] = -ball.velocity[1];
-            } else if ball_y <= ball.radius && ball.velocity[1] < 0.0 {
+            if ball_y >= ARENA_HEIGHT - ball.radius && ball.velocity[1] > 0.0
+                || ball_y <= ball.radius && ball.velocity[1] < 0.0
+            {
                 ball.velocity[1] = -ball.velocity[1];
             }
 
@@ -43,16 +43,14 @@ impl<'s> System<'s> for BounceSystem {
                 if point_in_rect(
                     ball_x,
                     ball_y,
-                    paddle_x - ball.radius,
-                    paddle_y - ball.radius,
-                    paddle_x + paddle.width + ball.radius,
-                    paddle_y + paddle.height + ball.radius,
-                ) {
-                    if paddle.side == Side::Left && ball.velocity[0] < 0.0 {
-                        ball.velocity[0] = -ball.velocity[0];
-                    } else if paddle.side == Side::Right && ball.velocity[0] > 0.0 {
-                        ball.velocity[0] = -ball.velocity[0];
-                    }
+                    paddle_x,
+                    paddle_y,
+                    paddle_x + paddle.width,
+                    paddle_y + paddle.height,
+                ) && (paddle.side == Side::Left && ball.velocity[0] < 0.0
+                    || paddle.side == Side::Right && ball.velocity[0] > 0.0)
+                {
+                    ball.velocity[0] = -ball.velocity[0];
                 }
             }
         }
